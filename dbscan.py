21e81fc221d4a62d4f1be9e59190dee0bda582dc
21-072 M. Mahdum Al-A'Lam <102146028+alammahdum8@users.noreply.github.com>
2023-06-06 12:33:54 +0700

    Update dbscan.py

--- dbscan.py
+++ dbscan.py
@@ -1,31 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 14 12:50:42 2020
-
-@author: HP
-"""
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-df = pd.read_csv("new.csv");
-df.isna().sum()
-df.head()
-df.info()
-customers=df.drop(columns=['Channel','Region'])
-array=customers.values
-array
+# Fungsi untuk mengupload file CSV
+def upload_csv(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except FileNotFoundError:
+        print("File tidak ditemukan.")
+    except Exception as e:
+        print("Terjadi kesalahan saat membaca file:", e)
 
-#####normalize data############
-from sklearn.preprocessing import StandardScaler
-stsclr=StandardScaler().fit(array)
-norm_data=stsclr.transform(array)
+# Memasukkan path file CSV yang ingin diupload
+file_path = input("Masukkan path file CSV: ")
 
-#############DBSCAN clustering#########
-from sklearn.cluster import DBSCAN
-model=DBSCAN(eps=0.8,min_samples=6).fit(norm_data)
-clusters=pd.DataFrame(model.labels_,columns=['clusters'])
-clusters.value_counts()
-#-1 cluster represents outliers which means we have 80 datapts as outliers
-final_data=pd.concat([clusters,df],axis=1)
-final_data.groupby(final_data.clusters).sum()
+# Memanggil fungsi upload_csv()
+dataframe = upload_csv(file_path)
+
+# Jika data berhasil diupload, tampilkan informasi dataset
+if dataframe is not None:
+    print("Dataset berhasil diupload. Informasi dataset:")
+    print(dataframe.head())  # Menampilkan beberapa baris pertama dataset
